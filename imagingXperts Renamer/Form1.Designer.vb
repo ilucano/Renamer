39d78e76622001f5aaf41cf1b51b3c09e31a7011
ilucano <ilucano@imagingxperts.com>
2014-12-06 17:03:27 -0500

    final release

--- imagingXperts Renamer/Form1.Designer.vb
+++ imagingXperts Renamer/Form1.Designer.vb
@@ -22,54 +22,33 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.splitC = New System.Windows.Forms.SplitContainer()
         Me.grpBox = New System.Windows.Forms.GroupBox()
         Me.btnSave = New System.Windows.Forms.Button()
         Me.txtFileName = New System.Windows.Forms.TextBox()
         Me.btnStart = New System.Windows.Forms.Button()
         Me.grpLocation = New System.Windows.Forms.GroupBox()
-        Me.chkFoldorFiles = New System.Windows.Forms.CheckBox()
+        Me.rdFolders = New System.Windows.Forms.RadioButton()
+        Me.rdDocs = New System.Windows.Forms.RadioButton()
+        Me.chkPutFilename = New System.Windows.Forms.CheckBox()
         Me.chkCreate = New System.Windows.Forms.CheckBox()
         Me.btnExam = New System.Windows.Forms.Button()
         Me.txtPath = New System.Windows.Forms.TextBox()
-        Me.web = New System.Windows.Forms.WebBrowser()
         Me.fldB = New System.Windows.Forms.FolderBrowserDialog()
-        Me.chkPutFilename = New System.Windows.Forms.CheckBox()
-        CType(Me.splitC, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.splitC.Panel1.SuspendLayout()
-        Me.splitC.Panel2.SuspendLayout()
-        Me.splitC.SuspendLayout()
+        Me.web = New System.Windows.Forms.WebBrowser()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.btnSaveF = New System.Windows.Forms.Button()
+        Me.btnLoadF = New System.Windows.Forms.Button()
         Me.grpBox.SuspendLayout()
         Me.grpLocation.SuspendLayout()
         Me.SuspendLayout()
         '
-        'splitC
-        '
-        Me.splitC.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.splitC.Location = New System.Drawing.Point(0, 0)
-        Me.splitC.Name = "splitC"
-        '
-        'splitC.Panel1
-        '
-        Me.splitC.Panel1.Controls.Add(Me.grpBox)
-        Me.splitC.Panel1.Controls.Add(Me.btnStart)
-        Me.splitC.Panel1.Controls.Add(Me.grpLocation)
-        '
-        'splitC.Panel2
-        '
-        Me.splitC.Panel2.Controls.Add(Me.web)
-        Me.splitC.Size = New System.Drawing.Size(828, 666)
-        Me.splitC.SplitterDistance = 268
-        Me.splitC.TabIndex = 0
-        '
         'grpBox
         '
-        Me.grpBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.grpBox.Controls.Add(Me.btnSave)
         Me.grpBox.Controls.Add(Me.txtFileName)
         Me.grpBox.Enabled = False
-        Me.grpBox.Location = New System.Drawing.Point(12, 211)
+        Me.grpBox.Location = New System.Drawing.Point(12, 235)
         Me.grpBox.Name = "grpBox"
         Me.grpBox.Size = New System.Drawing.Size(245, 97)
         Me.grpBox.TabIndex = 4
@@ -83,6 +62,7 @@ Partial Class frmMain
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(89, 33)
         Me.btnSave.TabIndex = 5
+        Me.btnSave.TabStop = False
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = True
         '
@@ -90,27 +70,27 @@ Partial Class frmMain
         '
         Me.txtFileName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtFileName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
         Me.txtFileName.Location = New System.Drawing.Point(6, 29)
         Me.txtFileName.Name = "txtFileName"
         Me.txtFileName.Size = New System.Drawing.Size(233, 20)
-        Me.txtFileName.TabIndex = 1
+        Me.txtFileName.TabIndex = 0
         '
         'btnStart
         '
-        Me.btnStart.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnStart.Location = New System.Drawing.Point(162, 172)
         Me.btnStart.Name = "btnStart"
         Me.btnStart.Size = New System.Drawing.Size(89, 33)
         Me.btnStart.TabIndex = 3
+        Me.btnStart.TabStop = False
         Me.btnStart.Text = "Start"
         Me.btnStart.UseVisualStyleBackColor = True
         '
         'grpLocation
         '
-        Me.grpLocation.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grpLocation.Controls.Add(Me.rdFolders)
+        Me.grpLocation.Controls.Add(Me.rdDocs)
         Me.grpLocation.Controls.Add(Me.chkPutFilename)
-        Me.grpLocation.Controls.Add(Me.chkFoldorFiles)
         Me.grpLocation.Controls.Add(Me.chkCreate)
         Me.grpLocation.Controls.Add(Me.btnExam)
         Me.grpLocation.Controls.Add(Me.txtPath)
@@ -121,17 +101,39 @@ Partial Class frmMain
         Me.grpLocation.TabStop = False
         Me.grpLocation.Text = "Location"
         '
-        'chkFoldorFiles
+        'rdFolders
         '
-        Me.chkFoldorFiles.AutoSize = True
-        Me.chkFoldorFiles.Checked = True
-        Me.chkFoldorFiles.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.chkFoldorFiles.Location = New System.Drawing.Point(6, 106)
-        Me.chkFoldorFiles.Name = "chkFoldorFiles"
-        Me.chkFoldorFiles.Size = New System.Drawing.Size(99, 17)
-        Me.chkFoldorFiles.TabIndex = 3
-        Me.chkFoldorFiles.Text = "Are Documents"
-        Me.chkFoldorFiles.UseVisualStyleBackColor = True
+        Me.rdFolders.AutoSize = True
+        Me.rdFolders.Enabled = False
+        Me.rdFolders.Location = New System.Drawing.Point(103, 106)
+        Me.rdFolders.Name = "rdFolders"
+        Me.rdFolders.Size = New System.Drawing.Size(59, 17)
+        Me.rdFolders.TabIndex = 6
+        Me.rdFolders.Text = "Folders"
+        Me.rdFolders.UseVisualStyleBackColor = True
+        '
+        'rdDocs
+        '
+        Me.rdDocs.AutoSize = True
+        Me.rdDocs.Checked = True
+        Me.rdDocs.Enabled = False
+        Me.rdDocs.Location = New System.Drawing.Point(6, 106)
+        Me.rdDocs.Name = "rdDocs"
+        Me.rdDocs.Size = New System.Drawing.Size(79, 17)
+        Me.rdDocs.TabIndex = 5
+        Me.rdDocs.TabStop = True
+        Me.rdDocs.Text = "Documents"
+        Me.rdDocs.UseVisualStyleBackColor = True
+        '
+        'chkPutFilename
+        '
+        Me.chkPutFilename.AutoSize = True
+        Me.chkPutFilename.Location = New System.Drawing.Point(6, 131)
+        Me.chkPutFilename.Name = "chkPutFilename"
+        Me.chkPutFilename.Size = New System.Drawing.Size(119, 17)
+        Me.chkPutFilename.TabIndex = 4
+        Me.chkPutFilename.Text = "Filename as Default"
+        Me.chkPutFilename.UseVisualStyleBackColor = True
         '
         'chkCreate
         '
@@ -166,54 +168,95 @@ Partial Class frmMain
         '
         'web
         '
-        Me.web.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.web.Location = New System.Drawing.Point(0, 0)
+        Me.web.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.web.Location = New System.Drawing.Point(272, 0)
         Me.web.MinimumSize = New System.Drawing.Size(20, 20)
         Me.web.Name = "web"
-        Me.web.Size = New System.Drawing.Size(556, 666)
-        Me.web.TabIndex = 0
+        Me.web.Size = New System.Drawing.Size(558, 662)
+        Me.web.TabIndex = 5
         '
-        'chkPutFilename
+        'btnDelete
         '
-        Me.chkPutFilename.AutoSize = True
-        Me.chkPutFilename.Location = New System.Drawing.Point(6, 131)
-        Me.chkPutFilename.Name = "chkPutFilename"
-        Me.chkPutFilename.Size = New System.Drawing.Size(119, 17)
-        Me.chkPutFilename.TabIndex = 4
-        Me.chkPutFilename.Text = "Filename as Default"
-        Me.chkPutFilename.UseVisualStyleBackColor = True
+        Me.btnDelete.Location = New System.Drawing.Point(162, 360)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(89, 33)
+        Me.btnDelete.TabIndex = 6
+        Me.btnDelete.TabStop = False
+        Me.btnDelete.Text = "Delete File"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(15, 213)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 15)
+        Me.lblStatus.TabIndex = 7
+        '
+        'btnSaveF
+        '
+        Me.btnSaveF.Location = New System.Drawing.Point(162, 459)
+        Me.btnSaveF.Name = "btnSaveF"
+        Me.btnSaveF.Size = New System.Drawing.Size(89, 33)
+        Me.btnSaveF.TabIndex = 8
+        Me.btnSaveF.TabStop = False
+        Me.btnSaveF.Text = "Save File"
+        Me.btnSaveF.UseVisualStyleBackColor = True
+        '
+        'btnLoadF
+        '
+        Me.btnLoadF.Location = New System.Drawing.Point(67, 459)
+        Me.btnLoadF.Name = "btnLoadF"
+        Me.btnLoadF.Size = New System.Drawing.Size(89, 33)
+        Me.btnLoadF.TabIndex = 9
+        Me.btnLoadF.TabStop = False
+        Me.btnLoadF.Text = "Load File"
+        Me.btnLoadF.UseVisualStyleBackColor = True
         '
         'frmMain
         '
+        Me.AcceptButton = Me.btnStart
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(828, 666)
-        Me.Controls.Add(Me.splitC)
+        Me.Controls.Add(Me.btnLoadF)
+        Me.Controls.Add(Me.btnSaveF)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.web)
+        Me.Controls.Add(Me.grpBox)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.grpLocation)
+        Me.KeyPreview = True
         Me.Name = "frmMain"
         Me.Text = "imagingXperts Renamer"
-        Me.splitC.Panel1.ResumeLayout(False)
-        Me.splitC.Panel2.ResumeLayout(False)
-        CType(Me.splitC, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.splitC.ResumeLayout(False)
         Me.grpBox.ResumeLayout(False)
         Me.grpBox.PerformLayout()
         Me.grpLocation.ResumeLayout(False)
         Me.grpLocation.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents splitC As System.Windows.Forms.SplitContainer
-    Friend WithEvents web As System.Windows.Forms.WebBrowser
     Friend WithEvents grpBox As System.Windows.Forms.GroupBox
     Friend WithEvents btnSave As System.Windows.Forms.Button
     Friend WithEvents txtFileName As System.Windows.Forms.TextBox
     Friend WithEvents btnStart As System.Windows.Forms.Button
     Friend WithEvents grpLocation As System.Windows.Forms.GroupBox
-    Friend WithEvents chkFoldorFiles As System.Windows.Forms.CheckBox
     Friend WithEvents chkCreate As System.Windows.Forms.CheckBox
     Friend WithEvents btnExam As System.Windows.Forms.Button
     Friend WithEvents txtPath As System.Windows.Forms.TextBox
     Friend WithEvents fldB As System.Windows.Forms.FolderBrowserDialog
     Friend WithEvents chkPutFilename As System.Windows.Forms.CheckBox
+    Friend WithEvents rdFolders As System.Windows.Forms.RadioButton
+    Friend WithEvents rdDocs As System.Windows.Forms.RadioButton
+    Friend WithEvents web As System.Windows.Forms.WebBrowser
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents btnSaveF As System.Windows.Forms.Button
+    Friend WithEvents btnLoadF As System.Windows.Forms.Button
 
 End Class
--- imagingXperts Renamer/Form1.vb
+++ imagingXperts Renamer/Form1.vb
@@ -5,10 +5,30 @@ Public Class frmMain
 
     Private _arrFiles() As String
     Private _intCurrent As Integer
+    Private _autocomplete As New AutoCompleteStringCollection()
 
     Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         Dim lngRC As Long
         lngRC = WritePrivateProfileString("General", "startpath", txtPath.Text, "appinfo.ini")
+    End Sub
+
+    Private Sub frmMain_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.F5
+                txtFileName.Text = "No Patient"
+                btnSave_Click(btnSave, New System.EventArgs)
+            Case Keys.F4
+                txtFileName.Text = "Verify Name"
+                btnSave_Click(btnSave, New System.EventArgs)
+            Case Keys.F1
+                txtFileName.SelectAll()
+            Case Else
+                txtFileName.Focus()
+        End Select
+    End Sub
+
+    Private Sub frmMain_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+
     End Sub
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim rcl As Integer
@@ -23,6 +43,26 @@ Public Class frmMain
         Else
             txtPath.Text = "C:\"
         End If
+        LoadAutoComplete()
+    End Sub
+    Private Sub LoadAutoComplete()
+        Dim dirF As New DirectoryInfo(txtPath.Text & "\READY\")
+        Dim filArr As DirectoryInfo() = dirF.GetDirectories()
+        Dim fri As DirectoryInfo
+
+        For Each fri In filArr
+            _autocomplete.Add(fri.Name)
+        Next fri
+
+        With txtFileName
+            .AutoCompleteCustomSource = _autocomplete
+            .AutoCompleteMode = AutoCompleteMode.SuggestAppend
+            .AutoCompleteSource = AutoCompleteSource.CustomSource
+            .Location = New Point(20, 20)
+            .Width = 240
+            .Visible = True
+        End With
+
     End Sub
     Private Sub btnExam_Click(sender As Object, e As EventArgs) Handles btnExam.Click
         fldB.SelectedPath = txtPath.Text
@@ -35,11 +75,16 @@ Public Class frmMain
             grpLocation.Enabled = False
             grpBox.Enabled = True
             sender.Text = "Stop"
+            Me.AcceptButton = btnSave
             GetAllFiles()
             GetNextFile()
+            Application.DoEvents()
+            lblStatus.Text = "File " & _intCurrent & " of " & (_arrFiles.Count - 1)
+            txtFileName.Focus()
         Else
             grpLocation.Enabled = True
             grpBox.Enabled = False
+            Me.AcceptButton = btnStart
             sender.Text = "Start"
         End If
     End Sub
@@ -62,7 +107,9 @@ Public Class frmMain
                 If chkPutFilename.Checked = True Then
                     txtFileName.Text = IO.Path.GetFileNameWithoutExtension(_arrFiles(_intCurrent))
                 End If
+                'pdfV.LoadFile(_arrFiles(_intCurrent))
                 web.Navigate(_arrFiles(_intCurrent))
+                lblStatus.Text = "File " & _intCurrent & " of " & (_arrFiles.Count - 1)
             Else
                 MessageBox.Show("No More Files", "imagingXperts LLC", MessageBoxButtons.OK, MessageBoxIcon.Error)
                 btnStart_Click(btnStart, New System.EventArgs)
@@ -71,18 +118,110 @@ Public Class frmMain
             MessageBox.Show("The selected Directory not Exists", "imagingXperts LLC", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
     End Sub
-
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim strNewName As String
+
+        strNewName = StrConv(txtFileName.Text, VbStrConv.ProperCase).Trim
         ' Mover el archivo a la carpeta READY
         Dim strPath As String
-        If txtFileName.Text.Trim.Length > 3 Then
+        If strNewName.Length > 3 Then
             If chkCreate.Checked = True Then
-                strPath = txtPath.Text & "\READY\" & txtPath.Text & "\"
+                strPath = txtPath.Text & "\READY\" & strNewName & "\"
             Else
                 strPath = txtPath.Text & "\READY\"
             End If
+
+            ' Si no existe el directorio lo creo
+            If Not My.Computer.FileSystem.DirectoryExists(strPath) = True Then
+                My.Computer.FileSystem.CreateDirectory(strPath)
+            End If
+
+            ' Si ya hay un archivo Igual le agrego un nro
+            If My.Computer.FileSystem.FileExists(strPath & strNewName & ".pdf") = True Then
+                Dim intCnt As Integer = 1
+                Do Until Not (My.Computer.FileSystem.FileExists(strPath & strNewName & " " & intCnt & ".pdf"))
+                    intCnt += 1
+                Loop
+                My.Computer.FileSystem.MoveFile(_arrFiles(_intCurrent), strPath & strNewName & " " & intCnt & ".pdf", False)
+            Else
+                My.Computer.FileSystem.MoveFile(_arrFiles(_intCurrent), strPath & strNewName & ".pdf", False)
+            End If
+            _autocomplete.Add(strNewName)
+            _intCurrent += 1
+            GetNextFile()
+            Application.DoEvents()
+            txtFileName.Focus()
         Else
             MessageBox.Show("The Patient name must be greater than 3 chars", "imagingXperts LLC", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
+    End Sub
+
+    Private Sub txtFileName_LostFocus(sender As Object, e As EventArgs) Handles txtFileName.LostFocus
+        Dim currentTextBox As TextBox = DirectCast(sender, TextBox)
+        'Set the text of the generic textbox to Proper Case
+        currentTextBox.Text = StrConv(currentTextBox.Text, VbStrConv.ProperCase)
+    End Sub
+
+    Private Sub pdfV_GotFocus(sender As Object, e As EventArgs)
+        txtFileName.Focus()
+    End Sub
+
+    Private Sub web_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles web.DocumentCompleted
+        txtFileName.Focus()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If MessageBox.Show("Are you sure to delete file?", "imagingXperts LLC", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = Windows.Forms.DialogResult.OK Then
+            If My.Computer.FileSystem.FileExists(_arrFiles(_intCurrent)) = True Then
+                My.Computer.FileSystem.DeleteFile(_arrFiles(_intCurrent))
+            End If
+            _intCurrent += 1
+            GetNextFile()
+            Application.DoEvents()
+        End If
+        txtFileName.Focus()
+    End Sub
+
+    Private Sub web_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles web.PreviewKeyDown
+        Select Case e.KeyCode
+            Case Keys.F5
+                txtFileName.Text = "No Patient"
+                btnSave_Click(btnSave, New System.EventArgs)
+            Case Keys.F4
+                txtFileName.Text = "Verify Name"
+                btnSave_Click(btnSave, New System.EventArgs)
+            Case Keys.F1
+                txtFileName.SelectAll()
+            Case Else
+                txtFileName.Focus()
+        End Select
+    End Sub
+
+    Private Sub btnLoadF_Click(sender As Object, e As EventArgs) Handles btnLoadF.Click
+        If My.Computer.FileSystem.FileExists(txtPath.Text & "\listado.txt") Then
+            Dim objReader As New System.IO.StreamReader(txtPath.Text & "\listado.txt")
+
+            Do While objReader.Peek() <> -1
+                _autocomplete.Add(objReader.ReadLine())
+            Loop
+
+        End If
+    End Sub
+
+    Private Sub btnSaveF_Click(sender As Object, e As EventArgs) Handles btnSaveF.Click
+        If My.Computer.FileSystem.FileExists(txtPath.Text & "\listado.txt") Then
+            If MessageBox.Show("Are you sure to overwrite this file?", "imagingXperts LLC", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = Windows.Forms.DialogResult.OK Then
+                SaveAutocomplete()
+            End If
+        Else
+            SaveAutocomplete()
+        End If
+    End Sub
+    Public Sub SaveAutocomplete()
+        Dim objWriter As New System.IO.StreamWriter(txtPath.Text & "\listado.txt", False)
+
+        For Each obj In _autocomplete
+            objWriter.WriteLine(obj.ToString)
+        Next
     End Sub
 End Class
